25e5beb10d7e0bafc49fe6cc63e1b456c7599c99
Eimear McCarthy <mccarthyeb@cardiff.ac.uk>
2023-09-18 16:40:14 +0100

    latest iteation of ase_interface.py

--- interface/ase_interface.py
+++ interface/ase_interface.py
@@ -1,24 +1,9 @@
 import numpy as np
 from ase.io import read, write
 from ase.visualize import view
-from ase.build import add_adsorbate
-from ase import atoms
-
-
-# All angles seem to be at 60*
-# Will take interface file as input: for simplicity might just use isolated Pd and O layers to start
-
-
-# view(atoms)
-
-# avector_length=(np.linalg.norm(cellvectors[0]))
-# bvectorlength=(np.linalg.norm(cellvectors[1]))
-# half_avector=(avector_length / 2)
-# xy_centre=np.array([half_avector, half_avector])
 
 def atom_set_finder(atoms, isymbol):
     cell_vectors = atoms.get_cell()
-    print(cell_vectors)
 
     a_vector = cell_vectors[0]
     b_vector = cell_vectors[1]
@@ -26,85 +11,70 @@ def atom_set_finder(atoms, isymbol):
     xy_plane_vector_2 = b_vector[:2]
     xy_plane_midpoint = 0.5 * (xy_plane_vector_1 + xy_plane_vector_2)
 
-    #print("Midpoint of the XY plane is", xy_plane_midpoint)
-
     closest_distance = float(9999)
     closest_index = None
 
     for iatom in range(0, len(atoms)):
-        #print(f"%d %s %10.6f %10.6f %10.6f" \
-        #      % (iatom, atoms.symbols[iatom], atoms.positions[iatom][0], \
-        #         atoms.positions[iatom][1], atoms.positions[iatom][2]))
         if isymbol in atoms.symbols[iatom]:
             distance_to_centre = np.linalg.norm(atoms.positions[iatom][:2] - xy_plane_midpoint)
             if distance_to_centre < closest_distance:
                 closest_index = iatom
                 closest_distance = distance_to_centre
-    hip_hip_array=[[atoms.positions[closest_index][0], \
-                    atoms.positions[closest_index][1], \
-                    atoms.positions[closest_index][2]]]
-    for secatom in range(0, len(atoms)):
-        if isymbol in atoms.symbols[secatom]:
-           dist=atoms.get_distance(closest_index, secatom, mic=True)
-           if 2.5 < dist < 3.5:
-             hip_hip_array.append([atoms.positions[secatom][0], \
-                                   atoms.positions[secatom][1], \
-                                   atoms.positions[secatom][2]])
-                
-    return hip_hip_array
 
+    hip_hip_array = [atoms.positions[closest_index]]
+    atom_array = [closest_index]
+
+    for secatom in range(0, len(atoms)):
+        if isymbol in atoms.symbols[secatom] and secatom != closest_index:  # Exclude the central Pd atom
+            dist = atoms.get_distance(closest_index, secatom, mic=True)
+            if 2.5 < dist < 3.5:
+                hip_hip_array.append(atoms.positions[secatom])
+                atom_array.append(secatom)
+
+    return hip_hip_array, atom_array
+
+def find_parallel_atoms(atoms, seed_indices, direction_vector, isymbol, perpendicular_range, tolerance=0.1):
+    parallel_atoms = []
+    seed_positions = [atoms.positions[i] for i in seed_indices]
+
+    norm_direction_vector = direction_vector / np.linalg.norm(direction_vector)
+
+    for iatom in range(len(atoms)):
+        if iatom in seed_indices:
+            continue
+
+        if isymbol in atoms.symbols[iatom]:
+            position = atoms.positions[iatom]
+            vector = position - seed_positions[0]
+            #projection might be useful: corresponds to opposite side of right angled triangle formed by bond and direction vector
+            projection = np.dot(vector, norm_direction_vector)
+            if -perpendicular_range <= projection <= perpendicular_range:
+                parallel_atoms.append(iatom)
+
+    return parallel_atoms
+
+def find_hexagonal_strip(atoms, seed_indices, seed_index, perpendicular_range, tolerance):
+    direction_vector = (atoms.positions[19] - atoms.positions[16])
+    # Tidy this up later and generalize if it works!
+    # Need to set it as the vector perpendicular to the desired strip for some reason
+    parallel_atoms = find_parallel_atoms(atoms, seed_indices, direction_vector, "Pd", perpendicular_range, tolerance)
+
+    return parallel_atoms
 
 atoms = read("Pd_O_isolated.cif")
-Pd_index = atom_set_finder(atoms, "Pd")
-print(Pd_index)
-O_index = atom_set_finder(atoms, "O")
-print(O_index)
-print(len(O_index))
+view(atoms)
+Pd_hip_hip, Pd_indices = atom_set_finder(atoms, "Pd")
+print("Pd_indices:", Pd_indices)
+O_hip_hip, O_indices = atom_set_finder(atoms, "O")
 
+seed_Pd_index = Pd_indices[0]  #This is the centre
+print("seed_Pd_index", seed_Pd_index)
 
+strip_atoms_indices = find_hexagonal_strip(atoms, Pd_indices, seed_Pd_index, perpendicular_range=3.0, tolerance=0.1)
+combined_indices = Pd_indices + strip_atoms_indices
+strip_atoms = atoms[combined_indices]
+write("strip_atoms.cif", strip_atoms)
+view_strip = read("strip_atoms.cif")
+view(view_strip)
 
-
-
-
-
-
-#
-
-
-#
-#
-#
-#
-#
-# view(pd_structure)
-# transformation_matrix = np.dot(pd_structure.cell.T, np.linalg.inv(zno_structure.cell.T))
-#
-# pd_aligned_cell = pd_structure.cell @ transformation_matrix.T
-# pd_aligned = pd_structure.copy()
-# pd_aligned.set_cell(pd_aligned_cell, scale_atoms=True)
-# view(pd_aligned)
-##Isn't quite working: add in tolerance?
-# tolerance = 0.001
-# for pd_atom in pd_aligned:
-#    for zno_atom in zno_structure:
-#        displacement_vector = pd_atom.position - zno_atom.position
-#        displacement_norm = np.linalg.norm(pd_aligned.get_distance(pd_atom.index, zno_atom.index, mic=True))
-#        if displacement_norm < tolerance:
-#            # Shift Pd atom to achieve the desired tolerance
-#            fractional_coords = pd_aligned.get_scaled_positions()[pd_atom.index]
-#            fractional_coords += zno_atom.position
-#            pd_atom.position = pd_aligned.get_positions(fractional=True)[pd_atom.index]
-#            break
-#
-#
-# epitaxial_system = zno_structure.copy()
-# for atom in pd_aligned:
-#    epitaxial_system.append(atom)
-#
-# write('heterointerface.in', epitaxial_system, format='xyz')
-# view(epitaxial_system)
-#
-##Not quite working as desired yet!
-#
-#
-#
+print("The strip of overlapping Pd hexagons contains %d atoms." % len(strip_atoms))
--- pymatgen/coherent_interface_builder_EBMC/heterointerface_stable.py
+++ pymatgen/coherent_interface_builder_EBMC/heterointerface_stable.py
@@ -17,9 +17,9 @@ view(testview)
 ZnO = Structure.from_file("ZnO_opt.cif")
 Pd = Structure.from_file("Pd_opt.cif")
 sub_analyzer = SubstrateAnalyzer()
-sub_analyzer.calculate(film=Pd, substrate=ZnO)
+sub_analyzer.calculate(film=ZnO, substrate=Pd)
 
-matches = list(sub_analyzer.calculate(film=Pd,substrate=ZnO))
+matches = list(sub_analyzer.calculate(film=ZnO,substrate=Pd))
 for match in matches:
     print(match.match_area, match.von_mises_strain)
 
@@ -27,10 +27,10 @@ print(matches[0].as_dict())
 
 zsl = ZSLGenerator(max_area=400, max_length_tol=0.005)
 
-cib = CoherentInterfaceBuilder (film_structure=Pd,
-                                substrate_structure=ZnO,
-                                film_miller=(1,1,1),
-                                substrate_miller=(0,0,1),
+cib = CoherentInterfaceBuilder (film_structure=ZnO,
+                                substrate_structure=Pd,
+                                film_miller=(0,0,1),
+                                substrate_miller=(1,1,1),
                                 zslgen=zsl)
 
 print(cib.terminations)
@@ -41,38 +41,40 @@ for termination in cib.terminations:
 print(cib_list)
 
 for cib_val in cib_list:
-    interfaces=list(cib.get_interfaces(termination= (cib_val), film_thickness=2, substrate_thickness=2.5))
-    interface=interfaces[0]
-    #nterface.translate_sites(range(len(interface)),[0,0,0])
-    print("Done!")
-    print(interface)
-    #Seems to be fine here: going up in 0.05 in fractional coordinates
-
-    positions = [site.coords for site in interface.sites]
-    symbols = [site.species_string for site in interface.sites]
-
-
-    lattice = interface.lattice.matrix
-
-    interface_atoms = Atoms(symbols=symbols, positions=positions, cell=lattice)
-
-    interface_atoms.translate([0, 0, 0])
-
-    print("Our interface has been created!")
-    print(interface_atoms)
-    cib_val_str = '_'.join(str(val) for val in cib_val)
-    # looking for directories! Oh no! Can be fixed using 're'.
-    cib_val_str = re.sub(r'[/:]', '-', cib_val_str)
-
-    cif_fileout = 'interfaceZn%s.cif' % cib_val_str
-    aims_fileout = 'interfaceZn%s.in' % cib_val_str
-    print("Writing cif file ", cif_fileout)
-    write(cif_fileout, interface_atoms, format='cif')
-    write(aims_fileout, interface_atoms, format='aims')
-
-    view(interface_atoms)
-    
-
-
-
-
+    interfaces=list(cib.get_interfaces(termination= (cib_val), film_thickness=2, substrate_thickness=2))
+    for iinterface in interfaces:
+      interface=iinterface
+      #nterface.translate_sites(range(len(interface)),[0,0,0])
+      print("Done!")
+      print(interface)
+      #Seems to be fine here: going up in 0.05 in fractional coordinates
+   
+      positions = [site.coords for site in interface.sites]
+      symbols = [site.species_string for site in interface.sites]
+   
+   
+      lattice = interface.lattice.matrix
+   
+      interface_atoms = Atoms(symbols=symbols, positions=positions, cell=lattice)
+   
+      interface_atoms.translate([0, 0, 0])
+   
+      print("Our interface has been created!")
+      print(interface_atoms)
+      cib_val_str = '_'.join(str(val) for val in cib_val)
+      # looking for directories! Oh no! Can be fixed using 're'.
+      cib_val_str = re.sub(r'[/:]', '-', cib_val_str)
+   
+      cif_fileout = 'interfaceZn%s.cif' % cib_val_str
+      aims_fileout = 'interfaceZn%s.in' % cib_val_str
+      print("Writing cif file ", cif_fileout)
+      write(cif_fileout, interface_atoms, format='cif')
+      write(aims_fileout, interface_atoms, format='aims')
+   
+      view(interface_atoms)
+      
+   
+   
+   
+   
+   
